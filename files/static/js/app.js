42cc6ecd76d02bbadf123d45be743c8f911c1e35
selinataleb <selina.taleb@mail.utoronto.ca>
2024-11-18 18:51:03 -0500

    update

--- files/static/js/app.js
+++ files/static/js/app.js
@@ -5,7 +5,7 @@
 
     // get the metadata field
 
-    let metadata = data.metada;
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
 
@@ -30,8 +30,8 @@
     }
 
 // function to build both charts
-function buildCharts(sample) {
-  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+    function buildCharts(sample) {
+    d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
 let samples = data.samples;
@@ -133,13 +133,13 @@ function init() {
     let firstSample = names[0];
 
     // Build charts and metadata panel with the first sample
-buildCharts(firstSample);
+    buildCharts(firstSample);
     buildMetadata(firstSample);
   });
 }
 
 // Function for event listener
-function optionChanged(newSample) {
+  function optionChanged(newSample) {
   console.log("New Sample selected:", newSample);
   // Build charts and metadata panel each time a new sample is selected
 
